8796502729a8af85574359cec41c023d6c0771bf
ambujbpl <ambujdubey89@gmail.com>
2018-04-08 15:56:01 +0530

    jQuery Promises for multiple callback

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-// Native promise api used for ES6 javascript
-function get(url){
-  return new Promise(function(resolve, reject){
-      var xhttp = new XMLHttpRequest();
-      xhttp.open("GET",url,true);
-      xhttp.onload = function(){
-        if(xhttp.status == 200){
-          resolve(JSON.parse(xhttp.response));
-        }else{
-          reject(xhttp.statusText);
-        }
-      };
-      xhttp.onerror = function(){
-        reject(xhttp.statusText);
-      };
-      xhttp.send();
-  });
-};
-
-var promise = get("employee.json");
-promise.then(function(emp){
-  console.log("Employee>>",emp);
-  return get("student.json");
+// Jquery Promisses
+$.get("employee.json").then(function(emp){
+  console.log("Employee",emp);
+  return $.get("student.json");
 }).then(function(stu){
   console.log("Student",stu);
-  return get("invigilator.json")
+  return $.get("invigilator.json");
 }).then(function(inv){
   console.log("Invigilator",inv);
-}).catch(function(error){
-  console.log(error);
+}).fail(function(error){
+  console.log(error.responseText);
 });
